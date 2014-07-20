3ec846c5939288bba5c9e8f7f9defb03e1a5e598
angie <angie@ubuntu.ubuntu-domain>
2014-07-21 00:40:19 +0900

    MarkDown 함수 일부 수정 & 불필요 함수 제거

--- src/main/java/com/forweaver/util/WebUtil.java
+++ src/main/java/com/forweaver/util/WebUtil.java
@@ -63,38 +63,10 @@ public class WebUtil {
 	}
 	
 	public static String markDownEncoder(String str) {
-		//마크 다운 코드가 들어가면 html 코드로 바뀜
-		/*# This is an H1						<h1>This is an H1</h1>
-		## This is an H2			-> 			<h2>This is an H2</h2>
-		### This is an H3						<h3>This is an H3</h3>*/
 		try {
-			str = new Markdown4jProcessor().process(str);
+			return new Markdown4jProcessor().process(str);
 		} catch (IOException e) {
 			return str;
 		}
-		return str;
 	}
-	public static OutputStream pptToImg(InputStream is){
-		// ppt 파일을 이미지로 출력해주는 메서드
-		// 때에 따라서 매개변수 타입과 반환값의 타입이 다를 수 있음.
-		return null;
-	}
-	
-	//hackDay - [Docking] 문서에 접속한 사용자 간 작업 위치 표시 
-	public static String ipToAddress(String ip){
-		LookupService lookup = null;
-		  
-		try {
-		   lookup = new LookupService("/home/go/GeoLiteCity.dat", LookupService.GEOIP_MEMORY_CACHE);
-		} catch(IOException e) {
-		   e.printStackTrace(System.out);
-		}
-		
-		System.out.println(lookup.getLocation("218.209.82.120").latitude);
-		System.out.println(lookup.getLocation("218.209.82.120").longitude);
-		System.out.println(lookup.getLocation("218.209.82.120").city);
-		System.out.println(lookup.getLocation("218.209.82.120").city);
-		return null;
-	}
-
 }
